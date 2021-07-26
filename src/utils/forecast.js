d921a06a0ce0633345429a3006d091e401d2f9b5
Vi5HaL <vishalrathod41@gmail.com>
2021-07-26 08:59:14 +0530

    Changed the temprature unit to celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=fdffbc544d681552de4b1a95a264afe5&query='+latitude+','+longitude+'&units=f';
+    const url = 'http://api.weatherstack.com/current?access_key=fdffbc544d681552de4b1a95a264afe5&query='+latitude+','+longitude;
     request({ url, json: true}, (error,{body}) =>{
         if (error){
             callback('weather stack API is not reachable !',undefined)
